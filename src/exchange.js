236ffcfcbbfc222476f9f3a4af6c5ee98e0788b2
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-04-24 09:10:40 -0700

    add initial getExchangeRate API call function

---
+++ src/exchange.js
@@ -0,0 +1,18 @@
+//API REQUEST AND DISPLAY RESULTS ONLY
+
+export class ExchangeService {
+  async getExchangeRate() {
+    try {
+      let response = await fetch(`https://prime.exchangerate-api.com/v5/${process.env.API_KEY}/latest/USD`);
+      let jsonifiedResponse;
+      if (response.ok && response.status === 200) {
+        jsonifiedResponse = await response.json();
+      } else {
+        jsonifiedResponse = false;
+      }
+      return jsonifiedResponse;
+    } catch (error) {
+      return false;
+    }
+  }
+}
