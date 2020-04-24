5d869183a48811b07ed7058b35595f6ba7f34b2b
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-04-24 09:31:45 -0700

    add initial front end logic for collecting and converting user input

--- src/main.js
+++ src/main.js
@@ -2,7 +2,27 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from "jquery";
+import { ExchangeService } from "./../src/exchange";
+
+//FORM INPUT ONLY
 
 $(document).ready(function() {
-  
-})
+  $("#convert").click(function() {
+    const amount = $("#amount").val();
+    const newCurrency = $("#new-currency").val();
+    
+    (async () => {
+      let exhangeService = new ExchangeService();
+      let response = await exhangeService.getExchangeInfo();
+      console.log(convertCurrency(response));
+    });
+
+    function convertCurrency(response) {
+      if (response) {
+        return amount * response.conversion_rates[newCurrency];
+      } else {
+        return "ERROR";
+      }
+    }
+  });
+});
