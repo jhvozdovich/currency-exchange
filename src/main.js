0121dde0ae33139bf1193b868f649734788a7a59
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-04-24 11:11:54 -0700

    add logic to convertCurrency for invalid currency request

--- src/main.js
+++ src/main.js
@@ -19,18 +19,23 @@ $(document).ready(function() {
     })();
 
     function convertCurrency(response) {
+      let rate = response.conversion_rates[newCurrency];
       if (response.result === "error") {
         $(".results").hide();
         $("#error-type").html("<h3>API ERROR</h3> <p>Sorry, your API request was not fufilled. Please check your key.</p>");
         $("#error-message").html(`<p>Error: ${response.error}</p>`);
         $(".errors").show();
-      } else if (response.result === "success") {
+      } else if (response.result === "success" && rate) {
         $(".errors").hide();
-        let rate = response.conversion_rates[newCurrency]
         let conversion = (amount * rate).toFixed(2);
         $("#conversion").text(conversion + " "  + newCurrency);
         $("#rate").text(rate);
         $(".results").show();
+      } else if (response.result === "success" && !rate) {
+        $(".results").hide();
+        $("#error-type").html("<h3>UNIT ERROR</h3> <p>Sorry, your API request was not fufilled. Please check your currency request.</p>");
+        $("#error-message").html(`<p>Error: ${response.error}</p>`);
+        $(".errors").show();
       } else {
         return "ERROR";
       }
