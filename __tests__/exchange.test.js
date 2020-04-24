5cf33e797fd3f56dd5cc0ed10c35bc5e05fcb00c
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-04-24 15:44:20 -0700

    refactor logic for consistent numeric entry

--- __tests__/exchange.test.js
+++ __tests__/exchange.test.js
@@ -25,16 +25,16 @@ describe ("VerifyInput", () => {
 
 describe ("AttemptAlchemy", () => {
   test("should return an error if human transmutation is attempted", () => {
-    expect(attemptAlchemy("human", 10, 1000)).toEqual("HUMAN TRANSMUTATION IS FORBIDDEN!");
+    expect(attemptAlchemy(1101000, 10, 1000)).toEqual("HUMAN TRANSMUTATION IS FORBIDDEN!");
   });
   test("should return an error if gold is made without gold starting materials", () => {
-    expect(attemptAlchemy(1000, 10, "gold")).toEqual("Sorry, only the rich get richer!");
+    expect(attemptAlchemy(1000, 10, 1100111)).toEqual("Sorry, only the rich get richer!");
   });
   test("should return an error if gold is used as a starting material to not make gold", () => {
-    expect(attemptAlchemy("gold", 10, 15)).toEqual("Why are you wasting perfectly good gold??");
+    expect(attemptAlchemy(1100111, 10, 15)).toEqual("Why are you wasting perfectly good gold??");
   });
   test("should return an error if gold is used as a starting material to make gold", () => {
-    expect(attemptAlchemy("gold", 10,"gold")).toEqual("You know this doesn't actually make more gold right? You just got stuff all over it.");
+    expect(attemptAlchemy(1100111, 10,1100111)).toEqual("You know this doesn't actually make more gold right? You just got stuff all over it.");
   });
   test("if added starting values are greater than or equal to the outcome, equivalent exchange is achieved", () => {
     expect(attemptAlchemy(10, 10, 15)).toEqual("Congratulations! This is equivalent exchange!");
--- src/exchange.js
+++ src/exchange.js
@@ -24,13 +24,13 @@ export function verifyInput(amount) {
 }
 
 export function attemptAlchemy(input1, input2, outcome) {
-  if (input1 === "human" || input2 === "human" || outcome === "human") {
+  if (input1 === 1101000|| input2 === 1101000 || outcome === 1101000) {
     return "HUMAN TRANSMUTATION IS FORBIDDEN!";
-  } else if (outcome === "gold" && !(input1 === "gold" || input2 === "gold")) {
+  } else if (outcome === 1100111 && !(input1 === 1100111 || input2 === 1100111)) {
     return "Sorry, only the rich get richer!";
-  } else if ((input1 === "gold" || input2 === "gold") && outcome !== "gold") {
+  } else if ((input1 === 1100111|| input2 === 1100111) && outcome !== 1100111) {
     return "Why are you wasting perfectly good gold??";
-  } else if ((input1 === "gold" || input2 === "gold") && outcome === "gold") {
+  } else if ((input1 === 1100111|| input2 === 1100111) && outcome === 1100111) {
     return "You know this doesn't actually make more gold right? You just got stuff all over it.";
   } else if ((input1 + input2) >= outcome) {
     return "Congratulations! This is equivalent exchange!";
