24b57c310e4e3b059447fcc07843b9fc8896ff1b
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-04-24 14:05:42 -0700

    update UI and Business logic to allow for non USD exchanges

--- src/exchange.js
+++ src/exchange.js
@@ -1,7 +1,7 @@
 export class ExchangeService {
-  async getExchangeInfo() {
+  async getExchangeInfo(currency) {
     try {
-      let response = await fetch(`https://prime.exchangerate-api.com/v5/${process.env.API_KEY}/latest/USD`);
+      let response = await fetch(`https://prime.exchangerate-api.com/v5/${process.env.API_KEY}/latest/${currency}`);
       let jsonifiedResponse;
       if (response.ok && response.status === 200) {
         jsonifiedResponse = await response.json();
@@ -16,7 +16,7 @@ export class ExchangeService {
 }
 
 export function verifyInput(amount) {
-  if(isNaN(amount) || amount <= 0) {
+  if (isNaN(amount) || amount <= 0) {
     return false;
   }  else {
     return true;
--- src/main.js
+++ src/main.js
@@ -4,25 +4,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import { ExchangeService } from "./../src/exchange";
 import { verifyInput } from "./../src/exchange";
-//FORM INPUT ONLY
 
 $(document).ready(function() {
   $("#convert").click(function(event) {
     event.preventDefault();
     const amount = $("#amount").val();
+    const inputCurrency = $("#input-currency").val();
     const newCurrency = $("#new-currency").val();
 
     if (verifyInput(amount)) {
       (async () => {
         let exhangeService = new ExchangeService();
-        let response = await exhangeService.getExchangeInfo();
+        let response = await exhangeService.getExchangeInfo(inputCurrency);
         
         convertCurrencyDisplay(response);
       })();
     } else {
       $(".results").hide();
       $("#error-type").html("<h3>INPUT ERROR</h3>");
-      $("#error-message").html(`<p>Please enter a valid positive number.</p>`);
+      $("#error-message").html("<p>Please enter a valid positive number.</p>");
       $(".errors").show();
     }
 
@@ -35,21 +35,21 @@ $(document).ready(function() {
         $("#error-type").html("<h3>API ERROR</h3> <p>Sorry, your API request was not fufilled. Please check your key.</p>");
         $("#error-message").html(`<p>Error: ${response.error}</p>`);
         $(".errors").show();
-      } else if (response.result === "success" && response.conversion_rates[newCurrency]) {
+      } else if (response.result === "success" && response.conversion_rates[newCurrency] && response.conversion_rates[inputCurrency]) {
         let rate = response.conversion_rates[newCurrency];
         $(".errors").hide();
         let conversion = (amount * rate).toFixed(2);
         $("#conversion").text(conversion + " "  + newCurrency);
         $("#rate").text(rate);
         $(".results").show();
-      } else if (response.result === "success" && !response.conversion_rates[newCurrency]) {
+      } else if (response.result === "success" && (!response.conversion_rates[newCurrency] || !response.conversion_rates[inputCurrency])) {
         $(".results").hide();
         $("#error-type").html("<h3>UNIT ERROR</h3> <p>Sorry, your API request was not fufilled. Please check your currency request.</p>");
         $("#error-message").html(`<p>Error: ${response.error}</p>`);
         $(".errors").show();
       } else {
         $("#error-type").html("<h3>ERROR</h3>");
-        $("#error-message").html(`<p>Error: An unexpected error occured</p>`);
+        $("#error-message").html(`<p>Error: An unexpected error occured</p> <p>Error:${response}</p>`);
         $(".errors").show();
       }
     }
